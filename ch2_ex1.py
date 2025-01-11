833dadeed8178535f599b214e2ca8c8b39b62666
sabaflz <sabaa.feilizadeh@gmail.com>
2025-01-10 17:09:51 -0800

    ch2 ex1

---
+++ ch2_ex1.py
@@ -0,0 +1,24 @@
+# Ch.2, Ex.1
+# Saba Feilizadeh
+# Compute gross pay using hours and rate per hour
+
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+pay = hours * rate
+print("Pay: ", pay)
+
+
+'''
+Outputs:
+
+Enter Hours: 37
+Enter Rate: 24.25
+Pay:  897.25
+
+------------------
+
+Enter Hours: 12
+Enter Rate: 17.25
+Pay:  207.0
+
+'''
